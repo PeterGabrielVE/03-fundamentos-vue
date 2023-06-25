3c61738fe326651dee76289407587deabac09c66
Pedro Gabriel <31576642+PeterGabrielVE@users.noreply.github.com>
2023-06-25 19:09:41 -0400

    primer componentes

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,12 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Holaaaa "/>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
 }
 </script>
 
