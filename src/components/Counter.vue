85f01345e7dfd3f970e6494d88434fa52d13499b
Pedro Gabriel <31576642+PeterGabrielVE@users.noreply.github.com>
2023-07-04 12:56:13 -0400

    simular eventos

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -2,7 +2,8 @@
   <h2>{{ customTitle }}</h2>
 
   <p>{{ counter }} <sup>2</sup> = {{ squareCounter }} </p>
-  <p>{{ counter }}</p>
+
+  <p data-testid="counter">{{ counter }}</p>
   <div class="buttons-container">
         <button @click="increase">+1</button>
         <button @click="decrease">-1</button>
--- tests/unit/components/counter.spec.js
+++ tests/unit/components/counter.spec.js
@@ -29,9 +29,28 @@ describe('Counter Component',() => {
         
         const pTags = wrapper.findAll('p');
 
-        console.log(pTags)
     })
     
+    test('debe incrementar y decrementar el valor del contador', async() => {
+      
+        const wrapper = shallowMount( Counter );
+        const increaseBtn = wrapper.find('button');
+
+        await increaseBtn.trigger('click')
+
+        let value = wrapper.find('[data-testid="counter"]').text()
+
+        expect( value ).toBe('101');
+
+        const dereaseBtn = wrapper.find('button')[1];
+
+        await dereaseBtn.trigger('click')
+        await dereaseBtn.trigger('click')
+
+        value = wrapper.find('[data-testid="counter"]').text()
+
+        expect( value2 ).toBe('99');
+    })
     
     
     
