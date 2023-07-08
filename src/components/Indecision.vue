85285185c453ddb0c22ac9eb5df3d009f6927858
Pedro Gabriel <31576642+PeterGabrielVE@users.noreply.github.com>
2023-07-08 17:17:08 -0400

    definir las pruebas a realizar

--- src/components/Indecision.vue
+++ src/components/Indecision.vue
@@ -46,6 +46,8 @@ export default {
 
         this.isValidQuestion = false
 
+        console.log({ value })
+
         if( !value.includes('?') ) return
 
         this.isValidQuestion = true
--- tests/unit/components/indecision.spec.js
+++ tests/unit/components/indecision.spec.js
@@ -15,5 +15,22 @@ describe('Indecision component', () =>{
 
     })
 
+    test('Escribir en el input no debe de disparar nada console.log', () =>{
+
+    })
+
+    test('Escribir el simbolo  de "?" debe de disparar el fetch', () =>{
+
+    })
+
+    test('pruebas en getAnswer', () =>{
+
+    })
+
+    test('pruebas en getAnswer - Fallo en el API', () =>{
+
+    })
+
+
 
 })
