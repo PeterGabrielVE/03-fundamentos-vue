d019d708218db1478d852c7d172c8bb8383093de
Pedro Gabriel <31576642+PeterGabrielVE@users.noreply.github.com>
2023-07-07 21:08:05 -0400

    enviar props y evaluarlas

--- tests/unit/components/counter.spec.js
+++ tests/unit/components/counter.spec.js
@@ -63,6 +63,18 @@ describe('Counter Component',() => {
 
         expect( Number(value) ).toBe( start )
     })
+
+    test('debe de mostrar la prop title',()=>{
+      
+        const title = 'Hola Mundo!!!!'
+        const wrapper = shallowMount( Counter, {
+            props: {
+                title
+            }
+        } )
+
+        expect( wrapper.find( 'h2' ).text() ).toBe( title )
+    })
     
     
     
