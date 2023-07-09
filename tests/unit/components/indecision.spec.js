4339b75ab77139c95219d27f0cee3e4a09b31f26
Pedro Gabriel <31576642+PeterGabrielVE@users.noreply.github.com>
2023-07-08 20:01:21 -0400

    pruebas sobre el fetch api

--- tests/unit/components/indecision.spec.js
+++ tests/unit/components/indecision.spec.js
@@ -6,7 +6,13 @@ describe('Indecision component', () =>{
     let wrapper;
     let clgSpy;
 
-    global.fetch = jest.fn()
+    global.fetch = jest.fn( () => Promise.resolve({
+        json: () => Promise.resolve({
+            "answer": "yes",
+            "forced": false,
+            "image": "https://yesno.wtf/assets/yes/7-653c8ee5d3a6bbafd759142c9c18d76c.gif"
+        })
+    }) )
 
     beforeEach( () =>{
         wrapper = shallowMount( Indecision )
@@ -45,8 +51,14 @@ describe('Indecision component', () =>{
 
     })
 
-    test('pruebas en getAnswer', () =>{
+    test('pruebas en getAnswer', async() =>{
 
+        await wrapper.vm.getAnswer()
+
+        const img = wrapper.find('img')
+
+        expect( img.exists() ).toBeTruthy()
+        expect( wrapper.vm.img ).toBe('https://yesno.wtf/assets/yes/7-653c8ee5d3a6bbafd759142c9c18d76c.gif')
     })
 
     test('pruebas en getAnswer - Fallo en el API', () =>{
